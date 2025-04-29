73204fcaf6735e1d2a7fc8d869fa9c50e56e45d8
jason <pochli6262@gmail.com>
2025-04-30 01:32:28 +0800

    Initial Commit

---
+++ SwiftUI_class_HW5/SwiftUI_class_HW5App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_class_HW5App.swift
+//  SwiftUI_class_HW5
+//
+//  Created by 廖柏鈞 on 2025/4/30.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_class_HW5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
